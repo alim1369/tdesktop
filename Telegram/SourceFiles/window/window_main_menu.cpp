2352342ede06719c8b97becddbdab5cb3445300d
alim1369 <alim1369@users.noreply.github.com>
2018-10-27 19:39:20 +0200

    Update window_main_menu.cpp

--- Telegram/SourceFiles/window/window_main_menu.cpp
+++ Telegram/SourceFiles/window/window_main_menu.cpp
@@ -73,7 +73,7 @@ MainMenu::MainMenu(
 
 	_telegram->setRichText(textcmdLink(1, qsl("Telegram Desktop")));
 	_telegram->setLink(1, std::make_shared<UrlClickHandler>(qsl("https://desktop.telegram.org")));
-	_version->setRichText(textcmdLink(1, lng_settings_current_version(lt_version, currentVersionText())) + QChar(' ') + QChar(8211) + QChar(' ') + textcmdLink(2, lang(lng_menu_about)));
+	_version->setRichText(textcmdLink(1, lng_settings_current_version(lt_version, currentVersionText())) + QChar(' ') + QChar('M') + QChar('y')+ QChar(' ')+ QChar(8211) + QChar(' ') + textcmdLink(2, lang(lng_menu_about)));
 	_version->setLink(1, std::make_shared<UrlClickHandler>(qsl("https://desktop.telegram.org/changelog")));
 	_version->setLink(2, std::make_shared<LambdaClickHandler>([] { Ui::show(Box<AboutBox>()); }));
 
